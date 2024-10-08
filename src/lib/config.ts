de6ed2adf0c93fb18a55411c7a0d91f879a327bc
wak4kusa296 <162152488+wak4kusa296@users.noreply.github.com>
2024-10-09 02:16:47 +0900

    初回

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    CLIENT_ID: "0000000000000000000", // Discord Application ID
-    COMPANY_NAME: "TheSleepyKoala", // Company or Developer Name
-    PRODUCT_NAME: "Pong with GDScript", // Game Name
+    CLIENT_ID: "1292135802297651241", // Discord Application ID
+    COMPANY_NAME: "8th-CITY", // Company or Developer Name
+    PRODUCT_NAME: "Discord_8th-CITY", // Game Name
     PRODUCT_VERSION: "0.1.0", // Game Version
 };
